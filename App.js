21379f1c94e7e4f13c9495606fba5d4afe59b616
yofa21ab <younesfahmi2@gmail.com>
2024-09-23 18:50:21 +0200

    Updated app

--- App.js
+++ App.js
@@ -6,25 +6,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import ActivitiesScreen from './screens/ActivitiesScreen';
 import ForumScreen from './screens/ForumScreen';
-import ProfileScreen from './components/ProfileScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
-// Firebase configuration
-import firebase from 'firebase/app';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC2HgM_YMsQYp8Y9assdq6gXZMrbniMM7Q",
-  authDomain: "obligatorisk-opgave-1.firebaseapp.com",
-  projectId: "obligatorisk-opgave-1",
-  storageBucket: "obligatorisk-opgave-1.appspot.com",
-  messagingSenderId: "751342484352",
-  appId: "1:751342484352:web:b780a6a7297b6eea40df65"
-};
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-} else {
-  firebase.app(); // hvis det allerede er initialiseret
-}
 
 const Tab = createBottomTabNavigator();
 
